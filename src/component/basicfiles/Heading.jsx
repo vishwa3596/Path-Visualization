b72cca8743752a768b7f45ab0fbb7d6d64c1fb2d
vishwajit kumar singh <vishwa3556@gmail.com>
2019-10-08 14:57:54 +0530

    started adding styling in header

--- src/component/basicfiles/Heading.jsx
+++ src/component/basicfiles/Heading.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Selectalgo from "./selectalgo";
 import "./heading.css";
 class Heading extends Component {
     // adding toogle and the information page along with about us and a demo video.
@@ -7,7 +6,14 @@ class Heading extends Component {
         return (
             <div className="header">
                 <h2>PATH VISUALIZER</h2>
-                <Selectalgo/>
+                <select>
+                    <option value="select the algorithm to visualize the path"></option>
+                        <option value="dijkstra">dijkstra</option>
+                        <option value="dfs">DFS</option>
+                        <option value="bfs">BFS</option>
+                        <option value="a*">A*</option>
+                </select>
+                <button>visualize algorithm</button>
             </div>
         );
     }
